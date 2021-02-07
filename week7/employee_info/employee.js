8639783642e541ee3e908f8119669a22c360f763
MelissaHoffman09 <melissa.hoffman@bryanuniversity.edu>
2021-02-06 23:47:27 -0800

    initial

---
+++ week7/employee_info/employee.js
@@ -0,0 +1,24 @@
+const employees = [];
+
+function Employee(name, jobTitle, salary, status = "Full Time") {
+    this.name = name
+    this.jobTitle = jobTitle
+    this.salary = salary
+    this.status = status
+}
+Employee.prototype.printEmployeeForm = function() {
+    console.log(this)
+}
+
+let kate = new Employee("Kate", "server", "$15/hr");
+let jill = new Employee("Jill", "bartender", "$18/hr");
+let nikki = new Employee("Nikki", "hostess", "$14/hr", "Part Time");
+
+kate.printEmployeeForm();
+jill.printEmployeeForm();
+nikki.printEmployeeForm();
+
+employees.push(kate, jill, nikki);
+
+console.log(employees);
+   
